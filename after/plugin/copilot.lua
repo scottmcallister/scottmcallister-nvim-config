657f6d5293f73b0ee7d5493b5b6698b6cd38f134
McAllister, Scott <smcallister@cvent.com>
2024-06-14 12:08:59 -0300

    remove duplicate code

--- after/plugin/copilot.lua
+++ after/plugin/copilot.lua
@@ -19,15 +19,3 @@ vim.keymap.set("n", "<leader>ct", ":CopilotToggle<CR>", { noremap = true, silent
 vim.keymap.set("n", '<leader>cc', vim.cmd.CopilotChat)
 vim.keymap.set("v", '<leader>cc', vim.cmd.CopilotChat)
 
-local copilot_on = true
-vim.api.nvim_create_user_command("CopilotToggle", function()
-	if copilot_on then
-		vim.cmd("Copilot disable")
-		print("Copilot OFF")
-	else
-		vim.cmd("Copilot enable")
-		print("Copilot ON")
-	end
-	copilot_on = not copilot_on
-end, { nargs = 0 })
-vim.keymap.set("n", "<leader>ct", ":CopilotToggle<CR>", { noremap = true, silent = true })
