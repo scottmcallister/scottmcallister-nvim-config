0c97b86336b90382c1fa4f4cc9ed0ed9c9ab16da
McAllister, Scott <smcallister@cvent.com>
2024-10-04 16:00:11 -0300

    set root directory for telescope

--- after/plugin/Telescope.lua
+++ after/plugin/Telescope.lua
@@ -1,5 +1,19 @@
+local function project_root()
+  local ok, nvim_lsp = pcall(require, 'nvim_lsp')
+  if not ok then
+    return nil
+  end
+  local root_dir = nvim_lsp.util.root_pattern('.git')(vim.fn.expand('%:p'))
+  return root_dir
+end
 local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader>f', builtin.find_files, {})
+vim.keymap.set('n', '<leader>f', builtin.find_files, {
+  cwd = project_root()
+})
 vim.keymap.set('n', '<leader>g', function()
-	builtin.grep_string({ search = vim.fn.input("Grep > ")});
+	builtin.grep_string({ 
+    search = vim.fn.input("Grep > "),
+    cwd = project_root()
+  });
 end)
+
